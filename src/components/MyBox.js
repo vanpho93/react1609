a76d3cbecd283d83d3b29ed793b1c2cf1ddc399f
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-30 11:29:00 +0700

    cmp interact

--- src/components/MyBox.js
+++ src/components/MyBox.js
@@ -4,14 +4,21 @@ export default class MyBox extends Component {
     constructor(props) {
         super(props);
         this.state = { value: 10 };
+        this.addValue = this.addValue.bind(this);
+    }
+
+    addValue() {
+        this.setState(prevState => ({ value: prevState.value + 1 }));
     }
 
     render() {
         const { value } = this.state;
+        // this.setState(prevState => ({ value: prevState + 1 }))
+        // this.setState({ value: this.state.value + 1 })
         return (
             <div>
                 <h3>{value}</h3>
-                <MyButton />
+                <MyButton onAdd={this.addValue} />
             </div>
         );
     }
@@ -20,7 +27,7 @@ export default class MyBox extends Component {
 class MyButton extends Component {
     render() {
         return (
-            <button className="btn btn-primary">
+            <button className="btn btn-primary" onClick={this.props.onAdd}>
                 Add
             </button>
         );
