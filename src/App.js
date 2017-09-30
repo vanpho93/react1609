c8f73cdc980b5c6ea3caa99a42253e026c2c6ca8
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-30 10:54:07 +0700

    toggle form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import List from './components/List';
-import Box from './components/Box';
+import List from './components/List';
+// import Box from './components/Box';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Box />
+        <List />
       </div>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -10,8 +10,9 @@ const words = [
 export default class List extends Component {
     constructor(props) {
         super(props);
-        this.state = { arrWords: words };
+        this.state = { arrWords: words, isShowForm: false };
         this.addWord = this.addWord.bind(this);
+        this.onShowForm = this.onShowForm.bind(this);
     }
 
     addWord() {
@@ -19,25 +20,41 @@ export default class List extends Component {
         const vn = this.refs.txtVn.value
         const isMemorized = this.refs.cbMemorized.checked;
         const word = { en, vn, isMemorized };
-        this.setState({ arrWords: this.state.arrWords.concat(word) });
+        this.setState({ 
+            arrWords: this.state.arrWords.concat(word),
+            isShowForm: false
+        });
         this.refs.txtEn.value = '';
         this.refs.txtVn.value = '';
         this.refs.cbMemorized.checked = false;
     }
 
+    onShowForm() {
+        this.setState({ isShowForm: true });
+    }
+
     render() {
-        const { arrWords } = this.state;
+        const { arrWords, isShowForm } = this.state;
         return (
             <div>
-                <input className="form-control" placeholder="English" ref="txtEn" />
-                <br />
-                <input className="form-control" placeholder="Vietnamese" ref="txtVn" />
-                <br />
-                <label><input type="checkbox" ref="cbMemorized" /> Memorized</label>
-                <br />
-                <button className="btn btn-primary" onClick={this.addWord}>
-                    Add word
-                </button>
+                { isShowForm ? (
+                    <div>
+                        <input className="form-control" placeholder="English" ref="txtEn" />
+                        <br />
+                        <input className="form-control" placeholder="Vietnamese" ref="txtVn" />
+                        <br />
+                        <label><input type="checkbox" ref="cbMemorized" /> Memorized</label>
+                        <br />
+                        <button className="btn btn-primary" onClick={this.addWord}>
+                            Add word
+                        </button>
+                    </div>
+                    ) : (
+                        <button className="btn btn-primary" onClick={this.onShowForm}>
+                            Show Form
+                        </button>
+                    )
+                }
                 { arrWords.map(word => <Word key={word.en} {...word} />) }
             </div>
         );
