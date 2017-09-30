2aef5204443bb16846a4616e70822e85b970dd1c
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-30 08:55:01 +0700

    isMemorized

--- src/App.js
+++ src/App.js
@@ -2,23 +2,11 @@ import React, { Component } from 'react';
 import Word from './components/Word';
 
 class App extends Component {
-  funcGetElement(isTrue) {
-    if (isTrue) {
-      return <p>Dung</p>;
-    }
-    return <p>Sai</p>;
-  }
   render() {
-    const element = false ? <p>Dung</p> : <p>Sai</p>;
-    const getElement = (isTrue) => isTrue ? <p>Dung</p> : <p>Sai</p>;
     return (
       <div>
-        {false ? <p>Dung</p> : <p>Sai</p>}
-        { element }
-        { getElement(true) }
-        { this.funcGetElement(true) }
-        <Word en="hello" vn="xin chào" />
-        <Word en="good morning" vn="chào buổi sáng" />
+        <Word en="hello" vn="xin chào" isMemorized={true} />
+        <Word en="good morning" vn="chào buổi sáng" isMemorized={false} />
       </div>
     );
   }
--- src/components/Word.js
+++ src/components/Word.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Word = ({ vn, en }) => (
-    <div>
-        <h3>{en}</h3>
-        <p>{vn}</p>
-    </div>
-);
+const Word = ({ vn, en, isMemorized }) => {
+    const color = isMemorized ? 'green' : 'red';
+    return (
+        <div>
+            <h3 style={{ color }}>{en}</h3>
+            <p>{vn}</p>
+        </div>
+    );
+};
 
 export default Word;
